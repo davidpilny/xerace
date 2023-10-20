d98070b67deeb99b6e80f7005c6c9b948d942a3b
david.pilny <david.pilny@brandembassy.com>
2023-10-20 16:16:47 +0200

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '67e3eaa6a7e6a0923324fda614df085934ef73e2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '67e3eaa6a7e6a0923324fda614df085934ef73e2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
